8ba5e0f1359f640a6e10e25de7cc8ee0472c4186
Luwam <luwamtmariam@gmail.com>
2023-05-02 20:46:43 -0500

    added api gateway

---
+++ ProductService/src/test/java/miu/edu/productservice/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package miu.edu.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
