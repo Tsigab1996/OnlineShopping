ab1365b66f8287d2f1fbd99036ddc0fe756de23e
Tsigab <107885237+Tsigab1996@users.noreply.github.com>
2023-05-05 21:35:51 -0500

    Eureka is added

--- ProductService/src/main/java/miu/edu/productservice/ProductServiceApplication.java
+++ ProductService/src/main/java/miu/edu/productservice/ProductServiceApplication.java
@@ -3,8 +3,9 @@ package miu.edu.productservice;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
-
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ProductServiceApplication {
 
--- ProductService/src/main/java/miu/edu/productservice/controller/ProductController.java
+++ ProductService/src/main/java/miu/edu/productservice/controller/ProductController.java
@@ -55,4 +55,6 @@ public class ProductController {
         return productService.updateProduct(productDTO);
     }
 
+
+
 }
